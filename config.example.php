f8ae42eb952a07ef422ffaa26be05c0213804373
Ellian Campos <ellian.campos12@gmail.com>
2024-04-24 22:57:27 -0600

    Add config example

---
+++ config.example.php
@@ -0,0 +1,10 @@
+<!-- COPIAR ESTE ARCHIVO COMO "config.php" Y COMPLETAR LAS VARIABLES-->
+
+<?php
+
+$host = "localhost";
+$database = "dbName";
+$user = "root";
+$password = "";
+
+$emailsUrl = "";
